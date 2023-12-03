564a0f27db07d23a33cc6f98ad2ce1411ac5b61d
menoudyann <Yann.MENOUD@cpnv.ch>
2023-12-03 17:09:34 +0100

    update  FaceData attributs

--- src/main/java/org/example/FaceData.java
+++ src/main/java/org/example/FaceData.java
@@ -5,17 +5,9 @@ import com.google.cloud.vision.v1.FaceAnnotation;
 import java.util.List;
 
 public class FaceData {
-    private float anger;
-    private float joy;
-    private float surprise;
-    private float confidence;
+    private List details;
 
     public FaceData(List<FaceAnnotation> faceAnnotations) {
-        for (FaceAnnotation faceAnnotation : faceAnnotations) {
-            this.anger = faceAnnotation.getAngerLikelihoodValue();
-            this.joy = faceAnnotation.getJoyLikelihoodValue();
-            this.surprise = faceAnnotation.getSurpriseLikelihoodValue();
-            this.confidence = faceAnnotation.getDetectionConfidence();
-        }
+        this.details = faceAnnotations;
     }
 }
