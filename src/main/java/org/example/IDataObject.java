fadbee5f0465c67b084776de06ad8853a62e3fa5
menoudyann <Yann.MENOUD@cpnv.ch>
2023-12-13 14:10:04 +0100

    create bucket interface

---
+++ src/main/java/org/example/IDataObject.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.File;
+import java.net.URL;
+
+public interface IDataObject {
+    public boolean doesObjectExist();
+    public void uploadObject(byte[] file, String remoteFullPath);
+    public File downloadObject(byte[] file, String localFullPath);
+    public URL publishObject(String remoteFullPath, int expirationTime);
+    public void removeObject(String remoteFullPath, boolean isRecursive);
+}
